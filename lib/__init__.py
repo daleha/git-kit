dad18454f634a2ecd8fccc8c246f31c6bf69fa7f
unknown <Dale@.(none)>
2011-12-11 23:31:37 -0600

    added windows support

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,6 +1,5 @@
 """
 Lib acts as the main bootstrap code for a python lib
-
 Important: call init with main as a function pointer argument for bootstrapping to occur
 """
 
@@ -31,17 +30,28 @@ def exclude(path):
 #recursively crawl a directory,appending all dirs rooted at BASEDIR to system path
 def append_all(dir=None):
 
+	def appendPath(path):
+		path = os.path.abspath(path)
+
+		if(sys.platform == "win32" ):
+			path = path.lower()
+		sys.path.insert(0,path)
+#		print "Appending "+path
+
+
 	
 	if (dir==None):
 		global BASEDIR
 		dir=os.path.join(BASEDIR,"lib")
-		sys.path.append(dir)
 
+	appendPath(dir)
+		
 	for each in os.listdir(dir):
-		path=os.path.join(dir,each)
+		path = os.path.join(dir,each)
+
 		if (os.path.isdir(path) and not exclude(path)):
-			sys.path.insert(0,path)
 			append_all(path)
+
 
 #Parse argdict is not yet finished. It does pre-processing on sys.argv, so that it passes an easier to use object to main
 def parse_argdict():
@@ -95,12 +105,12 @@ def boot_strap(main):
 	sys.argv[0]             
 
 	pathname = os.path.dirname(sys.argv[0])        
-	abspath= os.path.abspath(pathname)
+	append_all(pathname)
 
-	#print ('full path ='+ abspath)
+	print ('full path ='+pathname) 
 
-	BASEDIR=abspath
-	append_all()
+	BASEDIR=pathname
+
 	init(main)
 
 		
--- lib/pygit/__init__.py
+++ lib/pygit/__init__.py
@@ -14,12 +14,8 @@ __version__ = 'git'
 #{ Initialization
 def _init_externals():
 	"""Initialize external projects by putting them into the path"""
-	sys.path.append(os.path.join(os.path.dirname(__file__), 'ext', 'gitdb'))
-	
-	try:
-		import gitdb
-	except ImportError:
-		raise ImportError("'gitdb' could not be found in your PYTHONPATH")
+	#sys.path.append(os.path.join(os.path.dirname(__file__), 'ext', 'gitdb'))
+	import gitdb
 	#END verify import
 	
 #} END initialization
